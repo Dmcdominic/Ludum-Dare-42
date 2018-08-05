d590058a227fde5ecf833c166586f29d669bbfb4
Dmcdominic <dmcdominic@gmail.com>
2018-08-05 18:31:14 -0400

    Re-using some codebase from a previous project.

---
+++ Ludum Dare 42/Assets/Scripts/GM.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GM : MonoBehaviour {
+
+	public enum Scene { Init, MainMenu };
+
+	private static GM _instance;
+    public static GM Instance {
+        get {
+            if (_instance == null) {
+                _instance = new GM();
+            }
+            return _instance;
+        }
+    }
+	
+	private void Awake() {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+		} else {
+			_instance = this;
+		}
+		DontDestroyOnLoad(this);
+	}
+	
+	public static void changeScene(Scene scene) {
+		switch(scene) {
+			case (Scene.Init):
+				SceneManager.LoadScene(0);
+				break;
+			case (Scene.MainMenu):
+				SceneManager.LoadScene(1);
+				break;
+		}
+	}
+}
---
+++ Ludum Dare 42/Assets/Scripts/Menus & Settings/PauseManager.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour {
+
+	public GameObject HUD;
+	public GameObject PauseMenu;
+	public GameObject OptionsMenu;
+
+	private bool pressed;
+	private float previousTimeScale;
+
+	[HideInInspector]
+	public static bool paused = false;
+
+	// Singleton instance setup
+	private static PauseManager _instance;
+	public static PauseManager Instance { get { return _instance; } }
+
+
+	void Awake () {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			_instance = this;
+		}
+
+		/* if (transform.parent == null) {
+			DontDestroyOnLoad (this);
+		} */
+
+		previousTimeScale = 1.0f;
+		resume ();
+	}
+
+	void Update () {
+		// Pause keys
+		if (Input.GetAxisRaw("Pause") > 0) {
+			if (paused && !pressed) {
+				pressed = true;
+				resume ();
+			} else if (!paused && !pressed) {
+				pressed = true;
+				pause ();
+			} 
+		} else {
+			pressed = false;
+		}
+	}
+
+	// Pause the game
+	public void pause() {
+		paused = true;
+		previousTimeScale = Time.timeScale;
+		Time.timeScale = 0f;
+
+		HUD.SetActive (false);
+		PauseMenu.SetActive (true);
+		OptionsMenu.SetActive (false);
+	}
+
+	// Resume the game. If resetTimeScale, Time.timeScale is set back to 1
+	public void resume(bool resetTimeScale = true) {
+		paused = false;
+		if (resetTimeScale) {
+			//SlowMo.resetTimeScale();
+			previousTimeScale = 1;
+		} else {
+			Time.timeScale = previousTimeScale;
+		}
+
+		HUD.SetActive (true);
+		PauseMenu.SetActive (false);
+		OptionsMenu.SetActive (false);
+	}
+	
+}
