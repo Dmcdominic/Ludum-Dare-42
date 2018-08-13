926093690a23082b5d283ffc2bb5f1f78c6821b5
Dmcdominic <dmcdominic@gmail.com>
2018-08-13 00:52:07 -0400

    Added reset button. And lots of assets.

---
+++ Ludum Dare 42/Assets/Scripts/Persistent Game Control/FadeOverlayManager.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class FadeOverlayManager : MonoBehaviour {
+
+	private Animator animator;
+
+	[SerializeField]
+	private AnimationClip FadeToBlack;
+	[SerializeField]
+	private AnimationClip Loading;
+	[SerializeField]
+	private AnimationClip FadeFromBlack;
+
+	// Singleton management
+	private static FadeOverlayManager _instance;
+	public static FadeOverlayManager Instance { get { return _instance; } }
+
+	private void Awake() {
+		if (_instance != null && _instance != this) {
+			Destroy(this.gameObject);
+			return;
+		} else {
+			_instance = this;
+		}
+
+		if (transform.parent == null) {
+			DontDestroyOnLoad(this);
+		}
+
+		animator = GetComponent<Animator>();
+	}
+
+	public void fadeToBlack() {
+		animator.Play("FadeToBlack");
+	}
+
+	public void loading() {
+		animator.Play("Loading");
+	}
+
+	public void fadeFromBlack() {
+		animator.Play("FadeFromBlack");
+	}
+
+}
---
+++ Ludum Dare 42/Assets/Scripts/UI Management/ResetButton.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResetButton : MonoBehaviour {
+
+	public void resetLevel() {
+		if (GM.Instance.getGameState() != GameState.Playing) {
+			return;
+		}
+		GM.resetCurrentLevel();
+	}
+
+	private void Update() {
+		if (GM.Instance.getGameState() != GameState.Playing) {
+			return;
+		}
+		if (Input.GetAxisRaw("Reset") > 0) {
+			resetLevel();
+		}
+	}
+}
